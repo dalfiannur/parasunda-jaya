32b38afe066f92f1ca6ea82d3272ba49dbb59279
typeofname <dalfiannur@gmail.com>
2019-10-30 13:28:09 +0700

    Fix route /products

---
+++ src/data/index.js
@@ -0,0 +1,39 @@
+import BaksoDaging1 from '../images/bakso-daging-1.jpg'
+import BaksoAyam1 from '../images/bakso-ayam-1.jpg'
+import BaksoIkan1 from '../images/bakso-ikan-1.jpg'
+
+export const products = [
+  {
+    id: 1,
+    slug: 'bakso-daging',
+    name: 'Bakso Daging',
+    images: [
+      {
+        name: 'Bakso Daging',
+        path: BaksoDaging1
+      }
+    ]
+  },
+  {
+    id: 2,
+    slug: 'bakso-ayam',
+    name: 'Bakso Ayam',
+    images: [
+      {
+        name: 'Bakso Ayam',
+        path: BaksoAyam1
+      }
+    ]
+  },
+  {
+    id: 3,
+    slug: 'bakso-ikan',
+    name: 'Bakso Ikan',
+    images: [
+      {
+        name: 'Bakso Ikan',
+        path: BaksoIkan1
+      }
+    ]
+  }
+]
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import _ from 'lodash'
+import queryString from 'querystring'
+import { makeStyles } from '@material-ui/styles'
+import {
+	CssBaseline,
+	AppBar,
+	Toolbar,
+	Typography,
+	Grid
+} from '@material-ui/core'
+import { products } from '../data'
+
+const useStyle = makeStyles(theme => ({
+	content: {
+		paddingTop: 10
+	}
+}))
 
 export default () => {
+	const classes = useStyle()
+	const [slug, setSlug] = useState('bakso-daging')
+	const [product, setProduct] = useState(products[0])
+
+	useEffect(() => {
+		const url = _.replace(window.location.search, '?', '')
+		const query = queryString.parse(url)
+		setSlug(query.name ? query.name : 'bakso-daging')
+	}, [window.location.search])
+
+	useEffect(() => {
+		const index = products.findIndex(item => item.slug === slug)
+		setProduct(products[index])
+	}, [slug])
+
 	return (
 		<React.Fragment>
-			
+			<CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Pasundan Jaya
+          </Typography>
+        </Toolbar>
+      </AppBar>
+			<main className={classes.content}>
+				<Grid
+					container
+					spacing={3}
+					direction='row'
+					justify='center'
+					alignItems='flex-start' >
+					<Grid item md={4}>
+						<img src={product.images[0].path} alt={product.images[0].name} />
+					</Grid>
+					<Grid item md={6}>
+						{product.name}
+					</Grid>
+				</Grid>
+			</main>
 		</React.Fragment>
   )
 }
